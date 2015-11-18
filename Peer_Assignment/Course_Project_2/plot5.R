2f4735d026495743a9ef4fb4d669ed12bed4c90b
NortonLe <norton0704@yahoo.co.uk>
2015-11-18 23:39:27 +0700

    plotting 5 6

--- Peer_Assignment/Course_Project_2/plot5.R
+++ Peer_Assignment/Course_Project_2/plot5.R
@@ -3,11 +3,11 @@ library(dplyr)
 NEI <- readRDS("data/summarySCC_PM25.rds")
 SCC <- readRDS("data/Source_Classification_Code.rds")
 ##-----------------------------##
-sum_year <- filter(NEI, type == "on-road", fips == "24510") %>%
+sum_year <- filter(NEI, type == "ON-ROAD", fips == "24510") %>%
 		group_by(year) %>%
 		summarise(sum = sum(Emissions))
 ##-----------------------------##
-with(sum_year, barplot(height = sum, width = year), xlab = "Year", ylab = "Total emissions", main = "Total emissions in Baltimore\n from 1999-2008\n for all motor souces", col = "red"))		
+with(sum_year, barplot(height = sum, width = year, xlab = "Year", ylab = "Total emissions", main = "Total emissions in Baltimore\n from 1999-2008\n for all motor souces", col = "red"))		
 ##-----------------------------##
 dev.copy(png, "plot5.png")
 dev.off()
